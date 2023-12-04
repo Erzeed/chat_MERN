8e7d3451d8d0a49be5440ac1678142b3bb6dec23
Feizal Reza <feizal.reza@students.amikom.ac.id>
2023-12-04 21:30:33 +0700

    add ui chat

---
+++ src/components/card.jsx
@@ -0,0 +1,20 @@
+
+function Card() {
+    const img = "https://ui-avatars.com/api/?name=Elon+Musk&background=random&font-size=0.33"
+    return(
+        <div className="container_card h-[70px] ml-2 mr-2 flex justify-between items-center text-white p-3 cursor-pointer hover:bg-gray-700 hover:rounded-[15px]">
+            <img src={img} className="rounded-lg w-11 h-11" alt="profile" />
+            <div className="card_text ml-2 mr-2 items-between flex-1 ">
+                <div className="text_title antialiased">
+                    <p>Reza</p>
+                </div>
+                <div className="text_desc text-xs text-slate-400">
+                    <p>Saya sedang membuat App</p>
+                </div>
+            </div>
+            <div className="w-2 h-2 rounded-full bg-white m-auto"></div>
+        </div> 
+    )
+}
+
+export default Card;
